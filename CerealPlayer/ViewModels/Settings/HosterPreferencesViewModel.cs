646c81fe54d8eb5405dc3107e1a705a8257d6d8b
Felix <felixbruell@live.de>
2019-01-02 13:34:57 +0100

    added drag drop for hoster preferences

--- CerealPlayer/ViewModels/Settings/HosterPreferencesViewModel.cs
+++ CerealPlayer/ViewModels/Settings/HosterPreferencesViewModel.cs
@@ -7,14 +7,16 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using CerealPlayer.Annotations;
 using CerealPlayer.Commands;
 using CerealPlayer.Models.Hoster;
+using GongSolutions.Wpf.DragDrop;
 
 namespace CerealPlayer.ViewModels.Settings
 {
-    public class HosterPreferencesViewModel : INotifyPropertyChanged
+    public class HosterPreferencesViewModel : INotifyPropertyChanged, IDropTarget
     {
         public class HosterView
         {
@@ -59,6 +61,32 @@ namespace CerealPlayer.ViewModels.Settings
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void DragOver(IDropInfo dropInfo)
+        {
+            // enable if both items are HosterView
+            if (dropInfo.Data is HosterView && dropInfo.TargetItem is HosterView)
+            {
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+                dropInfo.Effects = DragDropEffects.Move;
+            }
+        }
+
+        public void Drop(IDropInfo dropInfo)
+        {
+            var item = (HosterView) dropInfo.Data;
+            var insertIndex = dropInfo.InsertIndex;
+            var oldIndex = Items.IndexOf(item);
+            Items.RemoveAt(oldIndex);
+            if (oldIndex >= insertIndex)
+            {
+                Items.Insert(insertIndex, item);
+            }
+            else // if(oldIndex < insertIndex)
+            {
+                Items.Insert(insertIndex - 1, item);
+            }
         }
     }
 }
